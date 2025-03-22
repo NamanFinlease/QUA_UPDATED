e7d7a1a04693f31e7fc16045a6594e37ca93390f
AjayOnly1Loan <ajay@only1loan.com>
2025-03-22 13:04:57 +0530

    working on master sheet

--- utils/MIS/master/masterSheet.js
+++ utils/MIS/master/masterSheet.js
@@ -18,8 +18,43 @@ export const getMasterSheet = async () => {
             },
         },
         {
-            $unwind: "$loanApplications",
+            $unwind: {
+                path: "$loanApplications",
+                preserveNullAndEmptyArrays: true,
+            },
         },
+
+        {
+            $lookup: {
+                from: "aadhaardetails",
+                localField: "aadhaar",
+                foreignField: "uniqueId",
+                as: "aadhaarDetails",
+            },
+        },
+        {
+            $unwind: {
+                path: "$aadhaarDetails",
+                preserveNullAndEmptyArrays: true,
+            },
+        },
+
+        {
+            $lookup: {
+                from: "pans",
+                let: { leadPan: "$pan" },
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: { $eq: ["$data.pan", "$$leadPan"] },
+                        },
+                    },
+                ],
+                as: "panDetails",
+            },
+        },
+        { $unwind: { path: "$panDetails", preserveNullAndEmptyArrays: true } },
+
         {
             $lookup: {
                 from: "camdetails",
@@ -28,20 +63,28 @@ export const getMasterSheet = async () => {
                 as: "camDetails",
             },
         },
-        {
-            $unwind: "$camDetails",
-        },
+        { $unwind: { path: "$camDetails", preserveNullAndEmptyArrays: true } },
         {
             $lookup: {
                 from: "documents",
-                localField: "documents", // This is a single ObjectId
-                foreignField: "_id",
-                as: "documents",
+                let: { docId: "$documents" },
+                pipeline: [
+                    {
+                        $match: {
+                            $expr: { $eq: ["$_id", "$$docId"] },
+                        },
+                    },
+                ],
+                as: "docs",
             },
         },
         {
-            $unwind: "$documents",
+            $unwind: {
+                path: "$docs",
+                preserveNullAndEmptyArrays: true,
+            },
         },
+
         {
             $lookup: {
                 from: "applicants",
@@ -58,8 +101,20 @@ export const getMasterSheet = async () => {
                 as: "applicants",
             },
         },
+        { $unwind: { path: "$applicants", preserveNullAndEmptyArrays: true } },
         {
-            $unwind: "$applicants",
+            $lookup: {
+                from: "banks",
+                localField: "applicants._id",
+                foreignField: "borrowerId",
+                as: "applicantBank",
+            },
+        },
+        {
+            $unwind: {
+                path: "$applicantBank",
+                preserveNullAndEmptyArrays: true,
+            },
         },
         {
             $lookup: {
@@ -69,9 +124,8 @@ export const getMasterSheet = async () => {
                 as: "application",
             },
         },
-        {
-            $unwind: "$application",
-        },
+        { $unwind: { path: "$application", preserveNullAndEmptyArrays: true } },
+
         {
             $lookup: {
                 from: "sanctions",
@@ -80,9 +134,8 @@ export const getMasterSheet = async () => {
                 as: "sanction",
             },
         },
-        {
-            $unwind: "$sanction",
-        },
+        { $unwind: { path: "$sanction", preserveNullAndEmptyArrays: true } },
+
         {
             $lookup: {
                 from: "disbursals",
@@ -91,10 +144,78 @@ export const getMasterSheet = async () => {
                 as: "disbursal",
             },
         },
+        { $unwind: { path: "$disbursal", preserveNullAndEmptyArrays: true } },
+
         {
-            $unwind: "$disbursal",
+            $project: {
+                updatedAt: 1,
+                fName: 1,
+                mName: 1,
+                lName: 1,
+                pan: 1,
+                mobile: 1,
+                leadNo: 1,
+                aadhaar: 1,
+                personalEmail: 1,
+                dob: 1,
+                cibilScore: 1,
+                fatherName: 1,
+                fathersName: 1,
+                mothersName: 1,
+                gender: 1,
+                pinCode: 1,
+                state: 1,
+                city: 1,
+                documents: 1,
+                "extraDetails.personalDetails.maritalStatus": 1,
+                "extraDetails.personalDetails.spouseName": 1,
+                "extraDetails.residenceDetails.residenceType": 1,
+                "extraDetails.incomeDetails.employmentType": 1,
+                "extraDetails.incomeDetails.monthlyIncome": 1,
+                "extraDetails.incomeDetails.workingSince": 1,
+                "extraDetails.incomeDetails.incomeMode": 1,
+                "loanApplications.loanDetails.loanPurpose": 1,
+                "loanApplications.loanDetails.principal": 1,
+                "loanApplications.loanDetails.tenure": 1,
+                "loanApplications.employeeDetails.workFrom": 1,
+                "loanApplications.employeeDetails.companyName": 1,
+                "loanApplications.employeeDetails.companyType": 1,
+                "loanApplications.employeeDetails.designation": 1,
+                "loanApplications.employeeDetails.officeEmail": 1,
+                "loanApplications.employeeDetails.officeAddrress": 1,
+                "loanApplications.employeeDetails.employedSince": 1,
+                "loanApplications.employeeDetails.pincode": 1,
+                "loanApplications.employeeDetails.city": 1,
+                "aadhaarDetails.details.address.house": 1,
+                "aadhaarDetails.details.address.street": 1,
+                "aadhaarDetails.details.address.landmark": 1,
+                "aadhaarDetails.details.address.loc": 1,
+                "aadhaarDetails.details.address.po": 1,
+                "aadhaarDetails.details.address.dist": 1,
+                "aadhaarDetails.details.address.vtc": 1,
+                "aadhaarDetails.details.address.pc": 1,
+                "aadhaarDetails.details.address.state": 1,
+                "aadhaarDetails.details.address.country": 1,
+                "panDetails.data.aadhaar_number": 1,
+                "applicants.residence.address": 1,
+                "applicants.residence.city": 1,
+                "applicants.residence.state": 1,
+                "applicants.residence.pincode": 1,
+                "applicants.residence.residingSince": 1,
+                "applicants.reference": {
+                    $slice: ["$applicants.reference", 2],
+                }, // ✅ Pick first 2 objects
+                "applicantBank.beneficiaryName": 1,
+                "applicantBank.bankAccNo": 1,
+                "applicantBank.ifscCode": 1,
+                "applicantBank.branchName": 1,
+                "applicantBank.bankName": 1,
+                "applicantBank.accountType": 1,
+                "applicantBank.isPennyDropped": 1,
+
+                // "applicant."
+            },
         },
     ]);
-
-    console.log("leads: ", leads);
+    return leads;
 };
